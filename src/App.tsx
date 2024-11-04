46708e31f5ac5b90d69dc6fc838fdc5f24d69da9
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-11-04 22:57:02 +0600

    home style change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import reactLogo from './assets/react.svg';
 
 function App() {
-
     return (
         <div className='App'>
-            <h1>Vite + Reactooooo</h1>
+            <section className='hero-section'>
+                <div className='hero-content'>
+                    <h1>Mahfujur Rahman</h1>
+                    <h2>Full Stack Developer</h2>
+                </div>
+                <div className='hero-image'>
+                    <img src={reactLogo} alt="React Logo" className='react-logo' />
+                </div>
+            </section>
         </div>
     );
 }

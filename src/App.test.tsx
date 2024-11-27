cdf480ed3abcdc4bd4889b4829f4c04a3f738af4
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-11-27 20:37:49 +0600

    test update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest';
 
 test('App includes "Mahfujur Rahman" text', async () => {
-  const app = await import('./App?raw');
+  const app = await import('./Pages/Home?raw');
   expect(app.default).toContain('Mahfujur Rahman');
 });
